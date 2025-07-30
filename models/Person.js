ed1153c5c427acea3b96f4479d1212856c27a611
sirothiya <shalini21sirothiya@gmail.com>
2025-07-30 22:35:57 +0530

    Adding Authentication and Authorization

--- models/Person.js
+++ models/Person.js
@@ -32,6 +32,16 @@ const personSchema=new mongoose.Schema({
     salary:{
         type:Number,
         required:true
+    },
+    username:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true,
+        minlength:6
     }
 })
 
--- server.js
+++ server.js
@@ -1,30 +1,62 @@
-const express=require('express');
-const app= express();
+const express = require("express");
+const app = express();
 
-require('dotenv').config()
-const PORT =process.env.PORT || 3000
+require("dotenv").config();
+const PORT = process.env.PORT || 3000;
 
-const db =require('./db');
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+// this is used to authenticate the user using local strategy and this local startegy is used to authenticate the user using username and password
 
-const bodyParser=require('body-parser');
+const db = require("./db");
+
+const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 //bodyParser is used to parse the request body
-//it is used to parse the json data sent in the request body    
+//it is used to parse the json data sent in the request body
+   const Person = require("./models/Person");
 
+const logRequest = (req, res, next) => {
+  console.log(
+    `[${new Date().toLocaleString()}] Request made to ${req.originalUrl}`
+  );
+  // console.log("hii i am a middleware")
+  next(); // this is used to call the next middleware in the stack
+};
+app.use(logRequest);
 
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      console.log("Received credentials:", username, password);
+      const user = await Person.findOne({ username });
+      if (!user) {
+        return done(null, false, { message: "User not exist" });
+      }
+      const isMatchPass = user.password === password;
+      if (!isMatchPass) {
+        return done(null, false, { message: "Incorrect password" });
+      }
+      return done(null, user);
+    } catch (err) {
+      console.error("Error during authentication:", err);
+      return done(err);
+    }
+  })
+);
 
-const PersonRoutes=require('./routes/PersonRoutes')
-const MenuRoutes=require('./routes/MenuRoutes')
+const PersonRoutes = require("./routes/PersonRoutes");
+const MenuRoutes = require("./routes/MenuRoutes");
 
-app.get('/',(req,res)=>{
-    res.send('Welcome to Hotel... we will try to serve you the best');
-})
+app.get("/",passport.authenticate("local",{session:false}), (req, res) => {
+  console.log("Root route hit");
+  res.send("Welcome to Hotel... we will try to serve you the best");
+});
 
-app.use('/person',PersonRoutes);
+app.use("/person", PersonRoutes);
 
-app.use('/menu',MenuRoutes)
+app.use("/menu", MenuRoutes);
 
-
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
